2937aaa4372ce9ac304f9f8edcd2549aed7d4dc6
Anyuu25 <anyuu25@gmail.com>
2023-06-06 22:36:54 +0500

    task 3

--- Homework9/task_3.py
+++ Homework9/task_3.py
@@ -5,4 +5,17 @@
 
 # Здесь пишем код
 
+with open("test_file/task_3.txt", 'r', encoding='utf-8') as file:
+    goods = []
+    good_price = 0
+    for price in file.readlines():
+        if price.isspace():
+            goods.append(good_price)
+            good_price = 0
+            continue
+        good_price += int(price)
+    goods.append(good_price)
+    sorted_prices = sorted(goods, reverse=True)
+    three_most_expensive_purchases = sum(sorted_prices[:3])
+
 assert three_most_expensive_purchases == 202346
