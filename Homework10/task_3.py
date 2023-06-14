f455e4f4e3c425e26cd40fbb3bc44ae91777fea3
Anyuu25 <anyuu25@gmail.com>
2023-06-14 22:53:31 +0500

    task 3

---
+++ Homework10/task_3.py
@@ -0,0 +1,24 @@
+# Из набора тестов задания task_2 создайте один тест с параметрами, используя @pytest.mark.parametrize
+# Промаркируйте 1 параметр из выборки как smokе, а 1 набор данных скипните
+
+import pytest
+
+
+def all_division(*arg1):
+    division = arg1[0]
+    for i in arg1[1:]:
+        division /= i
+    return division
+
+
+@pytest.mark.parametrize("args, output", [
+    pytest.param((1, 2), 0.5, marks=pytest.mark.smoke),
+    ((1, 2, 4), 0.125),
+    ((2, 0.5), 4),
+    ((0.5, 5), 0.1),
+    ((-5, 5), -1),
+    ((-5, -5), 1),
+    pytest.param((0), 5, marks=pytest.mark.skip(reason="Причина пропуска"))
+])
+def test_arguments(args, output):
+    assert all_division(*args) == output
